ab25ca0583486fd2883bf6fbbf76856c48a3fd53
banobepascal <banobepascal@gmail.com>
2020-01-01 14:57:13 +0300

    ft(tested jwt):
    - carried out a unit test on jwt
    - created test file and folder
    - made change to db.js
    
    Finishes

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,7 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 
-
 module.exports = function() {
-    mongoose.connect('mongodb://localhost/vidly')
+  mongoose.connect('mongodb://localhost/vidly')
   .then(() => winston.info('Connected to MongoDB...'))
-
 }
