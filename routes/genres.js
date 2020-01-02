c4d0e18422d217c48e9c3b6cef1ab3eea9031678
banobepascal <banobepascal@gmail.com>
2020-01-02 15:06:25 +0300

    ft(testing POST):
    - tested authorization
    - tested invalid inputs
    - tested happy paths
    - made some code refactoring
    
    Finishes

--- routes/genres.js
+++ routes/genres.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res, next) => {
     res.send(genres);
 });
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
--- tests/intergration/genres.test.js
+++ tests/intergration/genres.test.js
@@ -1,8 +1,11 @@
 const request = require("supertest");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Genre } = require("../../models/genre");
+const { User } = require("../../models/user");
 
 let server;
+
+// happy path
 
 describe("/api/genres", () => {
   beforeEach(() => {
@@ -32,16 +35,67 @@ describe("/api/genres", () => {
     it("should return a genre if valid Id", async () => {
       const genre = new Genre({ name: "genre1" });
       await genre.save();
- 
+
       const res = await request(server).get("/api/genres/" + genre._id);
       expect(res.status).toBe(200);
-      expect(res.body).toHaveProperty('name', genre.name);
+      expect(res.body).toHaveProperty("name", genre.name);
     });
 
     it("should return 404 if genre Id is invalid", async () => {
-        const res = await request(server).get("/api/genres/1");
-        expect(res.status).toBe(404);
-        // expect(res.body).toHaveProperty('name', genre.name);
-      });
+      const res = await request(server).get("/api/genres/1");
+      expect(res.status).toBe(404);
+      // expect(res.body).toHaveProperty('name', genre.name);
+    });
+  });
+
+  describe("POST /", () => {
+    let token;
+    let name;
+
+    const exec = async () => {
+      return await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name });
+    };
+
+    beforeEach(() => {
+      token = new User().generateAuthToken();
+      name = "genre1";
+    });
+
+    it("should return 401 if client is not logged in", async () => {
+      token = "";
+      const res = await exec();
+      expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre is less than 5 characters", async () => {
+      name = "1234";
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if genre is more than 50 characters", async () => {
+      name = new Array(52).join("a");
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should save the genre if valid", async () => {
+      await exec();
+
+      const genre = await Genre.find({ name: "genre1" });
+      expect(genre).not.toBeNull();
+    });
+
+    it("should have genre in the body", async () => {
+      const res = await exec();
+
+      expect(res.body).toHaveProperty("_id");
+      expect(res.body).toHaveProperty("name", "genre1");
+    });
   });
 });
